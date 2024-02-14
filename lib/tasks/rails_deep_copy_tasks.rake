fec327910348ddf13217f40cf7682af806902bf6
Emil Kampp <emil@kampp.me>
2024-02-14 22:24:21 +0100

    Install rspec

--- lib/tasks/rails_deep_copy_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :rails_deep_copy do
-#   # Task goes here
-# end
--- test/rails_deep_copy_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RailsDeepCopyTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, RailsDeepCopy
-  end
-end
